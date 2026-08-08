83c9f2552b6a0a562fcd9a1f7a0a5dfc4d438bad
kazact <kazact95@gmail.com>
2026-08-07 22:42:18 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,48 @@
+"""
+CHIP-8 Interpreter, implemented in python, via pyqt6.
+"""
+
+from PyQt6.QtWidgets import QApplication, QMainWindow
+import pprint
+
+font = bytes([0xF0, 0x90, 0x90, 0x90, 0xF0,
+              0x20, 0x60, 0x20, 0x20, 0x70,
+              0xF0, 0x10, 0xF0, 0x80, 0xF0,
+              0xF0, 0x10, 0xF0, 0x10, 0xF0,
+              0x90, 0x90, 0xF0, 0x10, 0x10,
+              0xF0, 0x80, 0xF0, 0x10, 0xF0,
+              0xF0, 0x80, 0xF0, 0x90, 0xF0,
+              0xF0, 0x10, 0x20, 0x40, 0x40,
+              0xF0, 0x90, 0xF0, 0x90, 0xF0,
+              0xF0, 0x90, 0xF0, 0x10, 0xF0,
+              0xF0, 0x90, 0xF0, 0x90, 0x90,
+              0xE0, 0x90, 0xE0, 0x90, 0xE0,
+              0xF0, 0x80, 0x80, 0x80, 0xF0,
+              0xE0, 0x90, 0x90, 0x90, 0xE0,
+              0xF0, 0x80, 0xF0, 0x80, 0xF0,
+              0xF0, 0x80, 0xF0, 0x80, 0x80
+              ])
+RAM = None
+"""4kB of 'RAM'"""
+
+PC = None
+"""12 bit address pointing to current instruction in memory. Actually 16 bits, but never uses more than 12."""
+
+INDEX_REGISTER = None
+""" 16 bit index register"""
+
+SUBROUTINE_STACK = None
+"""Stack that holds 16 bit addresses pointing to subroutines(functions)"""
+
+DELAY_TIMER = 0
+"""An 8-bit delay timer which is decremented at a rate of 60 Hz (60 times per second) until it reaches 0"""
+
+SOUND_TIMER = 0
+"""An 8-bit sound timer which functions like the delay timer, but which also gives off a beeping sound as long as it’s not 0"""
+
+REGISTERS = None
+"""16 8-bit gen purpose registers. VF used for flags."""
+
+if __name__ == '__main__':
+    a = bytearray(16)
+    print(a)
